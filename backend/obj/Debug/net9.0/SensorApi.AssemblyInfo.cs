ec4e550e76282cbaf33c618d3929d0d9616ab5d7
skrasikovs <jobkrasikovs@gmail.com>
2025-10-06 08:28:49 +0300

    Remove ESP32Test on python

--- backend/obj/Debug/net9.0/SensorApi.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/SensorApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SensorApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+239ceb5c0ee8f059ddb07d20694b91c52d9be45e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ade038f590d07ef49885c100b848634c87ebac")]
 [assembly: System.Reflection.AssemblyProductAttribute("SensorApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SensorApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
