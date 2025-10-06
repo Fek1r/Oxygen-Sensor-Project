f5e9ee49ba5a9e4be27a0cffea7e7a05b46dd7a2
skrasikovs <jobkrasikovs@gmail.com>
2025-10-06 15:58:41 +0300

    Add offline version for ESP32

--- backend/obj/Debug/net9.0/SensorApi.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/SensorApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SensorApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4582ebea29d9188ccdf58b0d94d663df6916f1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfeb21e24227951f0418a7b8c23c8f30150271fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("SensorApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SensorApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
